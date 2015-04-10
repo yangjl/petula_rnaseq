38420687645357c39ccf6bc12b99b678952de96b
yangjl0930@gmail.com <yangjl0930@gmail.com>
2015-04-10 12:01:46 -0700

    set up qc

---
+++ profiling/1.A.1_qc.R
@@ -0,0 +1,30 @@
+### Jinliang Yang
+### Feb 20th, 2015
+
+### input
+fastqfile = "slurm-scripts/sample.txt"
+
+### output scripts
+#Note: the samplefile must named as *.txt
+run_qc <- function(miniq=25, minip =50, samplefile=""){
+    
+    
+    #shfile = "largedata/step1_qc.sh"
+    basefile <- gsub("\\.txt$", "", samplefile)
+    shfile <- paste0(samplefile, "_qc.sh")
+    slurmfile <- paste0(samplefile, "_qc_run.sh")
+    ### int passes to fastq_quality_filer Minimum quality score to keep
+    #q = 25
+    ### Minimum percent of bases that must have [-q] quality
+    #p = 50
+    
+    ######################################################################
+    source("lib/PE_qc.R")
+    PE_qc(fqfile = fastqfile, shfile = shfile, q = miniq, p = minip)
+    
+    source("lib/setUpslurm.R")
+    setUpslurm(slurmsh=slurmfile, 
+               codesh= paste("module load FASTX-Toolkit/0.0.13.2-goolf-1.4.10", paste("sh", shfile), sep="\n"),
+               wd=NULL, jobid= gsub(".*\\/", "", basefile), email=TRUE)
+}
+
